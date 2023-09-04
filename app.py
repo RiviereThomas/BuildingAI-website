f216d3818b37fb411c6465bbb3677090472879a5
Thomas RIVIERE <riviere-thomas@outlook.fr>
2023-09-04 18:00:52 +0200

    on a la carte mais toujours le pblème avec les refresh

--- app.py
+++ app.py
@@ -1,28 +1,61 @@
 import folium
 import streamlit as st
-
+from  data import  get_list_building_df,prepare_df
 from streamlit_folium import st_folium
+from ipyleaflet import Map, GeoJSON
+import geopandas as gpd
+import ipywidgets as widgets
+from IPython.display import display
+
+
+import ipywidgets as widgets
+from IPython.display import display
+
+from shapely.geometry import MultiPolygon,Point,Polygon
+from shapely.wkt import loads
+from shapely.ops import unary_union
+
+
+from shapely import wkt, ops
+from pyproj import Transformer
+from tqdm import tqdm
+
+@st.cache_data
+def load_data():
+    # Chargez votre DataFrame ici
+    # Par exemple, si vous chargez à partir d'un fichier CSV :
+    df = prepare_df('75111')
+    return df
+
+
+@st.cache(allow_output_mutation=True)
+def create_interactive_map(df):
+    # Créez une carte Folium
+    center =[48.863218, 2.377144]
+    m = folium.Map(location=center, zoom_start=14, zoom_control=False)  # Coordonnées du centre de la carte
+
+
+    # Ajoutez les polygones géographiques à la carte
+    for index, row in df.iterrows():
+         geojson = row['geom_groupe_geo']  # Assurez-vous que votre DataFrame a une colonne 'geometry' avec des données GeoJSON ou WKT
+         folium.GeoJson(geojson).add_to(m)
+    return m
+
+# Appelez la fonction pour charger le DataFrame (elle sera mise en cache) puis charger la maps
+data = load_data().head(20)
+interactive_map = create_interactive_map(data)
+
 
 '''
 # Building AI front
 '''
 
-st.markdown('''
-  markdown
-''')
 
-#48.859932, 2.343623 paris centre
 
-# center on Liberty Bell, add marker
-m = folium.Map(location=[48.859932, 2.343623], zoom_start=12)
 
+# Affichez la carte dans Streamlit
+st.markdown("## Carte des Polygones Géographiques")
 
 # call to render Folium map in Streamlit
-st_data = st_folium(m, width=725)
-
-
-#url = 'https://taxifare.lewagon.ai/predict'
-
-#if url == 'https://taxifare.lewagon.ai/predict':
-
-#    st.markdown('Maybe you want to use your own API for the prediction, not the one provided by Le Wagon...')
+st_folium(interactive_map, width=725)
+#st.write(interactive_map, width=725)
