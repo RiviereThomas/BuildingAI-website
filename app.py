97a65022088bb9294a287b0d7dc4bfaf53ae14d8
Thomas RIVIERE <riviere-thomas@outlook.fr>
2023-09-04 11:15:15 +0200

    instanciation fichiers

---
+++ app.py
@@ -0,0 +1,23 @@
+import streamlit as st
+
+'''
+# Building AI front
+'''
+
+st.markdown('''
+
+''')
+
+
+
+
+#url = 'https://taxifare.lewagon.ai/predict'
+
+#if url == 'https://taxifare.lewagon.ai/predict':
+
+#    st.markdown('Maybe you want to use your own API for the prediction, not the one provided by Le Wagon...')
+
+
+
+
+## Finally, we can display the prediction to the user
